84078363d9bf5e5d41b8a68f178341e4efc722fa
Angad-The-Coder <angad.bhargav@gmail.com>
2021-10-10 23:21:41 -0700

    Finishing ArrayOperations

--- ArrayOperations/ArrayTester.java
+++ ArrayOperations/ArrayTester.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ArrayTester {
@@ -27,13 +28,14 @@ public class ArrayTester {
         System.out.println("The standard deviation is: "+stdDev);
 
         double pct = at.percentWithinStdDev(1);
+        System.out.println("The percent within 1 standard deviation is: "+pct+"%");
 
     }
     /**
      * Print the contents of nums in a pretty way
      */
     private void print() {
-
+        System.out.println("The array is:\n" + Arrays.toString(nums));
     }
     /**prompt the user for the number of elements, the min element and 
      * max element to create.  Then create an array with that number of 
@@ -41,45 +43,95 @@ public class ArrayTester {
      * array  to nums (private instance field)
      */
     private void makeRandoms() {
-        System.out.println("Enter how many items you would like to have in nums:");
-
         Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter how many items you would like to have in nums:");
         int length = input.nextInt();
+        System.out.println("Enter the smallest number you would like to have in nums:");
+        int minNum = input.nextInt();
+        System.out.println("Enter the largest number you would like to have in nums:");
+        int maxNum = input.nextInt();
+
         input.close();
 
         nums = new int[length];
         for(int i=0; i<nums.length; i++){
-            nums[i] = (int)(Math.random()*20+1);;
+            nums[i] = (int)(Math.random() * (maxNum - minNum) + minNum);
         }
     }
     /** find and return the minimum element in nums */
     private int findMin(){
+        if (nums.length == 0) return 0;
 
-        return 0;
+        int min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < min) min = nums[i];
+        }
+
+        return min;
     }
     /** find and return the maximum element in nums */
     private int findMax(){
+        if (nums.length == 0) return 0;
 
-        return 0;
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > max) max = nums[i];
+        }
+
+        return max;
     }
     /** find and return the mean of the elements in nums */
     private double findMean(){
+        if (nums.length == 0) return 0;
 
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+
+        return (double) sum / nums.length;
     }
     /** find and return the mode(s) of the elements in nums 
-     * Use the following algorith:
-     * 1.  find the min and max elements
-     * 2.  starting with the min, count how many times it occurred
-     * 3.  if it is best so far, make it the mode
-     * 4.  if it ties the best so far, "add" this element to the returned array
-     * 5.  either way, go on to the next number until we pass up max
      * @return an array with the modes.  If there is only one mode, the
      * array will have length 1
     */
     private int[] findModes(){
+        // maps numbers to how many times they occur in nums:
+        HashMap<Integer, Integer> numCount = new HashMap<>();
+        
+        // Find the highest count of any number(s) in nums (maxCount)
+        // and how many numbers occur maxCount times (numOfModes):
+        int maxCount = 0;
+        int numOfModes = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // Initialize the value of nums[i] with 0 if not
+            // seen already; will be incremented soon:
+            numCount.putIfAbsent(nums[i], 0);
 
-        return null;
+            numCount.put(nums[i], numCount.get(nums[i]) + 1);
+            if (numCount.get(nums[i]) > maxCount) {
+                maxCount = numCount.get(nums[i]);
+                numOfModes = 1;
+            }
+            else if (numCount.get(nums[i]) == maxCount) {
+                numOfModes++;
+            }
+        }
+
+        // Find entries in numCount that have a value of maxCount
+        // and add them to a modes array
+        int[] modes = new int[numOfModes];
+        int modeIdx = 0;
+        for (HashMap.Entry<Integer, Integer> entry : numCount.entrySet()) {
+            if (entry.getValue() == maxCount) {
+                modes[modeIdx] = entry.getKey();
+                modeIdx++;
+            }
+        }
+
+        // return the modes array:
+        return modes;
     }
     /** find and return the median of the elements in nums 
      * Use the following algorithm:
@@ -90,12 +142,31 @@ public class ArrayTester {
      *     will return the mean of the two middle elements.
     */
     private double findMedian(){
+        if (nums.length == 0) return 0;
 
-        return 0;
+        int[] sortedNums = nums.clone();
+        Arrays.sort(sortedNums);
+
+        int midIdx = sortedNums.length / 2;
+
+        if (sortedNums.length % 2 == 0) {
+            return (double) (
+                sortedNums[midIdx - 1] +
+                sortedNums[midIdx]
+            ) / 2;
+        }
+
+        return (double) sortedNums[midIdx];
     }
     private int countOdds(){
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 1) {
+                count++;
+            }
+        }
 
-        return 0;
+        return count;
     }
     /** Returns the standard deviation of the data set
      * Use the following algorithm:
@@ -111,8 +182,14 @@ public class ArrayTester {
      *     standard deviation.  So, return it!
     */
     private double calcStandardDev(){
+        double mean = this.findMean();
+        double stdDev = 0.0;
+        for (int i = 0; i < nums.length; i++) {
+            stdDev += Math.pow((nums[i] - mean), 2);
+        }
+        stdDev /= nums.length;
 
-        return 0;
+        return Math.sqrt(stdDev);
     }
     /** Returns the percent of data within the specified number
      * of standard deviations from the mean of the data set
@@ -130,9 +207,16 @@ public class ArrayTester {
      *     (num within stds of dev / size of array * 100)
     */
     private double percentWithinStdDev(double devs){
+        double mean = this.findMean();
+        double stdDev = this.calcStandardDev();
+        int numWithinStdDev = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (
+                nums[i] > (mean - devs * stdDev) &&
+                nums[i] < (mean + devs * stdDev)
+            ) numWithinStdDev++; 
+        }
 
-        return 0;
+        return (double) numWithinStdDev / nums.length;
     }
-    
-
 }
