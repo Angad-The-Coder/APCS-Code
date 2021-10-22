aed7c137e552ee086e708a09f9890f3d59d2b176
Angad-The-Coder <angad.bhargav@gmail.com>
2021-10-21 21:03:25 -0700

    Reorganizing HorseFRQs

---
+++ HorseFRQs/HorseBarnB/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+        String[] spaces1 = { "Trigger", null, "Silver", "Lady", null, "Patches", "Duke" };
+
+        consolidate(spaces1);
+
+        System.out.println(Arrays.toString(spaces1));
+    }
+
+    /**
+     * Consolidates the barn by moving horses so that the horses are in adjacent
+     * spaces, starting at index 0, with no empty space between any two horses.
+     * Postcondition: The order of the horses is the same as before the
+     * consolidation.
+     */
+    public static void consolidate(String[] spaces) {
+        int consolidatedHorses = 0;
+        for (int i = 0; i < spaces.length; i++) {
+            if (spaces[i] != null) {
+                if (i != consolidatedHorses) {
+                    spaces[consolidatedHorses] = spaces[i];
+                    spaces[i] = null;
+                }
+                consolidatedHorses++;
+            }
+        }
+    }
+}
