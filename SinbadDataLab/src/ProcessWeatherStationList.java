f50039a5554cd04991fd1f5d24d9a700528ea593
Angad-The-Coder <angad.bhargav@gmail.com>
2021-11-19 22:03:46 -0800

    Finish SinBadDataLab Project 3

--- SinbadDataLab/src/ProcessWeatherStationList.java
+++ SinbadDataLab/src/ProcessWeatherStationList.java
@@ -10,14 +10,95 @@ public class ProcessWeatherStationList {
    public static void main(String[] args) {
       DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/index.xml").load();
       ds.load();
-      ArrayList<WeatherStation> allstns =
-            ds.fetchList("WeatherStation", "station/station_name", "station/station_id");
-      System.out.println("Total stations: " + allstns.size());
+      ArrayList<WeatherStation> allStns =
+            ds.fetchList("WeatherStation", "station/station_name", "station/station_id",
+                  "station/state", "station/latitude", "station/longitude", "station/xml_url");
+      System.out.println("Total stations: " + allStns.size());
 
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter a state abbreviation: ");
-      String state = sc.next();
-      System.out.println("Stations in " + state);
+      String state = sc.nextLine();
 
+      // find all stations matching the user-inputted state:
+      ArrayList<WeatherStation> stateStations = stateStnList(allStns, state);
+      while (stateStations.size() == 0) {
+         // if there are no stations match the user-inputted states, continually prompt the user for
+         // a new state until it has stations in it:
+         System.out.println("There are no records matching that abbreviation; try again: ");
+         state = sc.nextLine();
+         stateStations = stateStnList(allStns, state);
+      }
+
+      // print out stats about the user-inputted states
+      System.out.println("Stations in " + state + ": " + stateStations.size());
+      System.out.println("The station with the highest latitude in " + state + " is: "
+            + maxLatitude(stateStations));
+      System.out.println("The station with the highest longitude in " + state + " is: "
+            + maxLongitude(stateStations));
+
+      sc.close();
+
+   }
+
+
+   /**
+    * Given an ArrayList of WeatherStations and a state abbreviation to find WeatherStations from,
+    * return an ArrayList containing all the WeatherStations found that are in the inputted state.
+    * 
+    * @param stationList ArrayList of WeatherStations
+    * @param state state abbreviation to find WeatherStations from
+    * @return an ArrayList containing all the WeatherStations found that are in the inputted state
+    */
+   private static ArrayList<WeatherStation> stateStnList(ArrayList<WeatherStation> stationList,
+         String state) {
+      ArrayList<WeatherStation> stateStations = new ArrayList<>();
+      for (WeatherStation station : stationList) {
+         if (station.getState().equals(state)) {
+            stateStations.add(station);
+         }
+      }
+      return stateStations;
+   }
+
+   /**
+    * Given an ArrayList of WeatherStations, return the WeatherStation with the highest latitude.
+    * 
+    * @param stationList ArrayList of WeatherStations
+    * @return the WeatherStation with the highest latitude
+    */
+   private static WeatherStation maxLatitude(ArrayList<WeatherStation> stationList) {
+      if (stationList.size() == 0)
+         return null;
+
+      WeatherStation maxStation = stationList.get(0);
+      for (int i = 1; i < stationList.size(); i++) {
+         WeatherStation currStation = stationList.get(i);
+         if (currStation.getLatitude() > maxStation.getLatitude()) {
+            maxStation = currStation;
+         }
+      }
+
+      return maxStation;
+   }
+
+   /**
+    * Given an ArrayList of WeatherStations, return the WeatherStation with the highest longitude.
+    * 
+    * @param stationList ArrayList of WeatherStations
+    * @return the WeatherStation with the highest longitude
+    */
+   private static WeatherStation maxLongitude(ArrayList<WeatherStation> stationList) {
+      if (stationList.size() == 0)
+         return null;
+
+      WeatherStation maxStation = stationList.get(0);
+      for (int i = 1; i < stationList.size(); i++) {
+         WeatherStation currStation = stationList.get(i);
+         if (currStation.getLongitude() > maxStation.getLongitude()) {
+            maxStation = currStation;
+         }
+      }
+
+      return maxStation;
    }
 }
--- SinbadDataLab/src/WeatherStation.java
+++ SinbadDataLab/src/WeatherStation.java
@@ -7,27 +7,80 @@ import java.util.ArrayList;
 public class WeatherStation {
    private String name;
    private String id;
-   private ArrayList<Observation> observations;
+   private String state;
+   private float latitude;
+   private float longitude;
+   private String observationURL;
 
    WeatherStation(String name, String id) {
       this.name = name;
       this.id = id;
    }
 
-   WeatherStation(String name, String id, String state) {
+   WeatherStation(String name, String id, String state, float latitude, float longitude,
+         String observationUrl) {
       this.name = name;
       this.id = id;
+      this.state = state;
+      this.latitude = latitude;
+      this.longitude = longitude;
+      this.observationURL = observationUrl;
    }
 
    /* Produce the id of this station */
    public String getId() {
-      return id;
+      return this.id;
    }
 
    /* Produce the name of this station */
    public String getName() {
-      return name;
+      return this.name;
    }
 
+   /* Produce the state of this station */
+   public String getState() {
+      return this.state;
+   }
 
+   /* Produce the latitude of this station */
+   public float getLatitude() {
+      return this.latitude;
+   }
+
+   /* Produce the longitude of this station */
+   public float getLongitude() {
+      return this.longitude;
+   }
+
+   /* Produce the longitude of this station */
+   public String getObservationUrl() {
+      return this.observationURL;
+   }
+
+   /**
+    * toString() method, including the WeatherStation's name and (latitude, longitude) coordinates.
+    * Negative latitude or longitude coordinates are made positive and indicated to be in their
+    * respective hemispheres.
+    */
+   public String toString() {
+      String stationStr = this.name + " (";
+      
+      // add latitude:
+      if (this.latitude < 0) {
+         // negative latitude --> southern hemisphere
+         stationStr += Math.abs(this.latitude) + "°S, ";
+      } else {
+         // positive latitude --> northern hemisphere
+         stationStr += this.latitude + "°N, ";
+      }
+
+      if (this.longitude < 0) {
+         // negative longitude --> western hemisphere
+         stationStr += Math.abs(this.longitude) + "°W)";
+      } else {
+         // positive longitude --> eastern hemisphere
+         stationStr += this.longitude + "°E)";
+      }
+      return stationStr;
+   }
 }
