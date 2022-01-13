aaa4ca9db7eeb17f20fd2695723873a2da99ddf4
Angad-The-Coder <angad.bhargav@gmail.com>
2022-01-13 14:35:08 -0800

    Add SwingGUI

---
+++ SwingGUI/MyFrame.java
@@ -0,0 +1,17 @@
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+    MyFrame() {
+        this.setTitle("Spotify");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(420, 420);
+        this.setVisible(true);
+
+        ImageIcon image = new ImageIcon("images/spotify-icon.png");
+        this.setIconImage(image.getImage());
+        this.getContentPane().setBackground(new Color(0, 0, 0));
+    }
+}
