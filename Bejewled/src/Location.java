1e4066c7462bd1d46939665f889caeb4491cacae
student <student@CSPB4s-MacBook-Air-4.local>
2022-01-24 14:20:17 -0800

    Add Bejewled

---
+++ Bejewled/src/Location.java
@@ -0,0 +1,9 @@
+
+/**
+ * This class encapsulates the row and column of a location
+ * in a 2D array.  Write the entire class using appropriate 
+ * class design concepts.
+ */
+public class Location {
+
+}
