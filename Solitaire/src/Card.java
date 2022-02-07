08af3dcb51812d73f3a995a5008759556dce1754
Angad Bhargav <angad.bhargav@gmail.com>
2022-02-07 14:18:17 -0800

    begin work on solitaire

---
+++ Solitaire/src/Card.java
@@ -0,0 +1,11 @@
+public class Card {
+    private String suit;
+    private String rank;
+    private boolean isShowing;
+
+    public Card(String suit, String rank, boolean isShowing) {
+        this.suit = suit;
+        this.rank = rank;
+        this.isShowing = isShowing;
+    }
+}
